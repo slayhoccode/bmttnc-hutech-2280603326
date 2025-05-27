2de1b2fe71761472b702328bb7237edfc912ddbe
phamt <phamtraidz@gmail.com>
2025-05-27 14:31:29 +0700

    thêm app

--- Lab-02/app.py
+++ Lab-02/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request
 from cipher.caesar import CaesarCipher
+from cipher.vigenere.vigenere_cipher import VigenereCipher
+from cipher.railfence.railfence_cipher import RailFenceCipher
+from cipher.playfair.playfair_cipher import PlayFairCipher
 
 app = Flask(__name__)
 
@@ -25,6 +28,68 @@ def caesar_decrypt():
     key = int(request.form['inputKeyCipher'])
     Caesar = CaesarCipher()
     decrypted_text = Caesar.decrypt_text(text, key)
+    return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+
+@app.route('/vigenere')
+def vigenere():
+    return render_template('vigenere.html')
+
+@app.route('/vigenere_encrypt', methods=['POST'])
+def vigenere_encrypt():
+    text = request.form['inputPlainText']
+    key = request.form['inputKeyPlain']
+    vigenere = VigenereCipher()
+    encrypted_text = vigenere.vigenere_encrypt(text, key)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+@app.route('/vigenere_decrypt', methods=['POST'])
+def vigenere_decrypt():
+    text = request.form['inputCipherText']
+    key = request.form['inputKeyCipher']
+    vigenere = VigenereCipher()
+    decrypted_text = vigenere.vigenere_decrypt(text, key)
+    return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
+
+@app.route('/railfence')
+def railfence():
+    return render_template('railfence.html')
+
+@app.route('/railfence_encrypt', methods=['POST'])
+def railfence_encrypt():
+    text = request.form['inputPlainText']
+    num_rails = int(request.form['inputNumRails'])
+    railfence = RailFenceCipher()
+    encrypted_text = railfence.rail_fence_encrypt(text, num_rails)
+    return f"text: {text}<br/>number of rails: {num_rails}<br/>encrypted text: {encrypted_text}"
+
+@app.route('/railfence_decrypt', methods=['POST'])
+def railfence_decrypt():
+    text = request.form['inputCipherText']
+    num_rails = int(request.form['inputNumRails'])
+    railfence = RailFenceCipher()
+    decrypted_text = railfence.rail_fence_decrypt(text, num_rails)
+    return f"text: {text}<br/>number of rails: {num_rails}<br/>decrypted text: {decrypted_text}"
+
+@app.route('/playfair')
+def playfair():
+    return render_template('playfair.html')
+
+@app.route('/playfair_encrypt', methods=['POST'])
+def playfair_encrypt():
+    text = request.form['inputPlainText']
+    key = request.form['inputKeyPlain']
+    playfair = PlayFairCipher()
+    matrix = playfair.create_playfair_matrix(key)
+    encrypted_text = playfair.playfair_encrypt(text, matrix)
+    return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
+
+@app.route('/playfair_decrypt', methods=['POST'])
+def playfair_decrypt():
+    text = request.form['inputCipherText']
+    key = request.form['inputKeyCipher']
+    playfair = PlayFairCipher()
+    matrix = playfair.create_playfair_matrix(key)
+    decrypted_text = playfair.playfair_decrypt(text, matrix)
     return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
 
 if __name__ == '__main__':
